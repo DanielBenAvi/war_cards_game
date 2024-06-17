589bb520ed2438a42c42bd22ddb53c02e5e87f3b
Daniel Ben Avi <daniel.ben.avi10@gmail.com>
2024-06-17 13:33:18 +0300

    Initial Commit

---
+++ war_cards_game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  war_cards_game
+//
+//  Created by דניאל בן אבי on 17/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
